32f5f91709539e5a7761fb53dbc8cb356f8e8e28
carlo montanaro <carloldn@outlook.com>
2018-10-15 21:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 var cart = [];
 
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
+function addToCart(item) { 
+let price =  Math.floor(Math.random() * 101)}
+let fruit = {[item]: price};
+  cart.push(fruit);
+  console.log(`${item} has been added to your cart.`);
   return cart;
-}
-
-function addToCart(item) {
  // write your code here
 }
 
 function viewCart() {
+   if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    let items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
+  }
+}
   // write your code here
 }
 
-function total() {
+function total() {function total() {
+  var pricePerItemInCart = [];
+  var totalPrice = 0
+  
+  for(var i = 0; i < cart.length; i++) {
+  var itemPrice = cart[i][Object.keys(cart[i])]
+    pricePerItemInCart.push(itemPrice)
+    }
+  
+  for (var j =0; j<pricePerItemInCart.length; j++) {
+    totalPrice = pricePerItemInCart[j]+=totalPrice
+  }
+  return totalPrice
+}
+total(cart)
   // write your code here
 }
 
